3c78253674d927e3e37a26e39d70952c8e767bfc
ANDREY FREITAS LOPES <andrey_lopes@estudante.sesisenai.org.br>
2024-02-21 17:24:56 -0300

    add routes

--- express.js
+++ express.js
@@ -6,45 +6,88 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) =>{
-    res.status(200).json({
-        textoo:"dadada"
-    })
+app.get("/", (req, res) => {
+  res.status(200).json({
+    textoo: "dadada"
+  })
 })
 
 
 
 app.get('/clientes', async (req, res) => {
-    try {
-      const clientes = await prisma.cliente.findMany();
-      res.status(200).json(clientes);
-    } catch (error) {
-      console.error('Erro ao buscar clientes:', error);
-      res.status(500).json({ success: false, message: 'Erro ao buscar clientes' });
-    }
-  })    
+  try {
+    const clientes = await prisma.cliente.findMany();
+    res.status(200).json(clientes);
+  } catch (error) {
+    console.error('Erro ao buscar clientes:', error);
+    res.status(500).json({ success: false, message: 'Erro ao buscar clientes' });
+  }
+})
 
 
-  app.post('/post', async (req, res) => {
-    const { name, fullname, cpf, address } = req.body
-    try{
+app.post('/post', async (req, res) => {
+  const { data } = req.body
+  console.log(req.body)
+  try {
 
-            const post = await prisma.cliente.create({
-            data: {
-                name:name,
-                fullname: fullname,
-                cpf:cpf,
-                address:address
-            },
-            })
-            res.status(200).json(post)
-    }catch(error){
+    const post = await prisma.cliente.create({
+      data: {
+        name: data.name,
+        fullname: data.fullname,
+        cpf: data.cpf,
+        address: data.address
+      },
+    })
+    res.status(200).json(post)
+  } catch (error) {
 
-        console.error('Erro ao criar cliente:', error);
-        res.status(500).json({ success: false, message: 'Erro ao criar cliente' });
-    }
-  })
+    console.error('Erro ao criar cliente:', error);
+    res.status(500).json({ success: false, message: 'Erro ao criar cliente' });
+  }
+})
 
+app.delete('/cliente/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+      // Use o Prisma para excluir o cliente com o ID especificado
+      const deletedCliente = await prisma.cliente.delete({
+          where: {
+              id: id // Certifique-se de converter o ID para um número, se necessário
+          }
+      });
+
+      res.status(200).json(deletedCliente);
+  } catch (error) {
+      console.error('Erro ao excluir cliente:', error);
+      res.status(500).json({ success: false, message: 'Erro ao excluir cliente' });
+  }
+});
+
+app.put('/cliente/:id', async (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body; 
+console.log(data)
+  try {
+    const updatedCliente = await prisma.cliente.update({
+      where: {
+        id: id
+      },
+      data: {
+        name: data.name||undefined,
+        fullname: data.fullname||undefined,
+        cpf: data.cpf||undefined,
+        address: data.address||undefined 
+      }
+    }); 
+
+    // Retorna uma resposta JSON com o cliente atualizado e status HTTP 200 (OK)
+    res.status(200).json(updatedCliente);
+  } catch (error) {
+    // Se ocorrer um erro durante a atualização do cliente, vai retornar    
+    console.error('Erro ao atualizar cliente:', error);
+    res.status(500).json({ success: false, message: 'Erro ao atualizar cliente' });
+  }
+});
 
 
 
@@ -52,4 +95,4 @@ app.get('/clientes', async (req, res) => {
 
 
 const port = 3000
-app.listen(port, ()=>console.log(`rodando na porta ${port}`))
+app.listen(port, () => console.log(`rodando na porta ${port}`))
